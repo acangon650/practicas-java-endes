bfcedb30e7897f46a873fe3bd3ec57396c508f6d
acangon650 <147538659+acangon650@users.noreply.github.com>
2023-12-06 16:52:14 +0100

    Create SaludoServlet.java

---
+++ Ejercicio1/SaludoServlet.java
@@ -0,0 +1,19 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class SaludoServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String nombre = request.getParameter("nombre");
+        String apellido = request.getParameter("apellido");
+        response.getWriter().println("¡Hola " + nombre + " " + apellido + "!")
+    }
+}
